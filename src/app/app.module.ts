3e286b52e63af0bfa7411b38df15803bb9c7225c
marimonto <marimontod@gmail.com>
2021-04-15 22:29:04 -0500

    chapter 11 Done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/components/star.component';
 import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
@@ -11,6 +13,7 @@ import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent
@@ -18,7 +21,14 @@ import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+     RouterModule.forRoot([
+       { path: 'products', component: ProductListComponent },
+       //{ path: 'products/:id', component: ProductDetailComponent },
+       { path: 'welcome', component: WelcomeComponent },
+       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+     ])
   ],
   bootstrap: [AppComponent]
 })
